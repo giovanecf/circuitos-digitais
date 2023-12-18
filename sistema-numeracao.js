0789411e2deeeb69dee3ac325a4daf8d9ad0f35a
Geovani <geovani537@gmail.com>
2023-12-18 10:37:08 -0300

    decToBin completed

--- sistema-numeracao.js
+++ sistema-numeracao.js
@@ -1,3 +1,26 @@
+function getHexDigit(d) {
+  const digits = [
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "A",
+    "E",
+    "C",
+    "D",
+    "E",
+    "F",
+  ];
+
+  return digits[d];
+}
+
 function binToDec(value = "") {
   if (typeof value !== "string" || value.length < 1)
     return "Unknow or not set value. Expected 'String' 1 or getter size long.";
@@ -22,21 +45,41 @@ function decToBin(value = "") {
     return "Unknow or not set value. Expected 'String' 1 or getter size long.";
 
   const converted_value_arr = [];
-  const value_as_int = parseInt(value);
-  const result = value_as_int + 1;
+  let numerator = parseInt(value);
 
-  let count = 0;
-  while (result > 1) {
-    result = value[count] / 2;
-    converted_value_arr.push(value[i] % 2);
-    count++;
+  while (numerator > 1) {
+    converted_value_arr.push(numerator % 2);
+    numerator = Math.floor(numerator / 2);
   }
 
-  return converted_value_arr.reverse();
+  converted_value_arr.push(numerator);
+
+  return converted_value_arr.reverse().join("");
 }
-function decToHex(value = "") {}
+function decToHex(value = "") {
+  if (typeof value !== "string" || value.length < 1)
+    return "Unknow or not set value. Expected 'String' 1 or getter size long.";
+
+  let converted_value = 0;
+  const num_of_elements = value.length - 1;
+
+  for (let i = 0; i < num_of_elements; i++) {
+    const reverse_index = num_of_elements - i;
+
+    converted_value += getHexDigit(
+      parseInt(value[reverse_index]) * Math.pow(16, i)
+    );
+  }
+
+  return converted_value;
+}
 
 function hexToBin(value = "") {}
 function hexToDec(value = "") {}
 
-console.log(decToBin("7"));
+console.log(decToHex(20));
+
+if (false)
+  for (let i = 0; i < 10; i++) {
+    console.log(decToBin(i.toString()));
+  }
